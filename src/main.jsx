5592c5f2edab8e74090cb39df21d9745c9c0d319
AbdullahDashti1 <abdullamustafadashti.0@gmail.com>
2025-12-27 00:43:44 +0300

    updated the css situation and removed app.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import './index.css'
 import { UserProvider } from './contexts/UserContext.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <BrowserRouter>
-        <UserProvider>
-          <App />
-        </UserProvider>
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>,
 )
